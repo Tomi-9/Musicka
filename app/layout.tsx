42312219438e0ddb9b1049631957061eb6ef703f
Temesgen <Tomi-9@users.noreply.github.com>
2026-09-06 11:34:08 +0300

    put analytics component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Analytics />
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
